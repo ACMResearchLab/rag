9fef3d8d698cfb4d6001996a6c97d61a9898102c
shoot649854 <jojoto8845@icloud.com>
2024-03-09 10:24:43 -0800

    🧹 Refactoring

--- src/gpt.py
+++ src/gpt.py
@@ -1,40 +1,25 @@
 import tiktoken
 import os
-import glob
-import json
 import dotenv
-
 from openai import OpenAI
-dotenv.load_dotenv()
 import logging
 
+dotenv.load_dotenv()
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 class GPT:
     def __init__(self, model='gpt-3.5-turbo') -> None:
         self.encoding = tiktoken.encoding_for_model(model) # gpt-3.5, gpt-3.5-turbo
-        self.API_KEY = os.environ.get('OPEN_AI_API_KEY')
-        pass
+        self.api_key = os.environ.get('OPEN_AI_API_KEY')
 
     def translate_code(self, code_segment: str):
-        prompt_message_system = "Translate the following code into English"
-        prompt_message_user = f"{code_segment}"
-        client = OpenAI(api_key=self.API_KEY)
+        return self._generate_text(code_segment)
 
-        completion = client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
-            messages=[
-                {"role": "system", "content": prompt_message_system},
-                {"role": "user", "content": prompt_message_user}
-            ]
-        )
-        return completion.choices[0].message.content
-
-    def generate_text(function):
+    def _generate_text(self, text):
         prompt_message_system = "Translate the following code into English"
-        prompt_message_user = f"{function}"
-        client = OpenAI(api_key='-')
+        prompt_message_user = f"{text}"
+        client = OpenAI(api_key=self.api_key)
         completion = client.chat.completions.create(
             model="gpt-3.5-turbo-1106",
             messages=[
@@ -45,6 +30,6 @@ class GPT:
         return completion.choices[0].message.content
 
     def tokenize_translated_text(self, code):
-        english = GPT.generate_text(code)
+        english = self._generate_text(code)
         tokens = self.encoding.encode(english)
         return tokens
